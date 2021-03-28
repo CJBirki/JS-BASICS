bc2dfde3eefd3a8a7fcf43db31c0c5c200a0d5da
Caroline <carolinebirkinshaw@outlook.com>
2021-03-28 22:12:43 +0100

    correct syntax error in variable

--- scripts/variable.js
+++ scripts/variable.js
@@ -1,7 +1,7 @@
 var bottlesOfLucozadeConsumed = 5;
 
-alert('Bottles of Lucozade that Caz has consumed: 'bottlesOfLucozadeConsumed)
+alert("Bottles of Lucozade that Caz has consumed:", bottlesOfLucozadeConsumed);
 
-console.log('Bottles of Lucozade Caz has consumed: 'bottlesOfLucozadeConsumed)
+console.log("Bottles of Lucozade that Caz has consumed:", bottlesOfLucozadeConsumed);
 
-document.write('Bottles of Lucozade that Caz has comnsumed: 'bottlesOfLucozadeConsumed)
+document.write("Bottles of Lucozade that Caz has comnsumed:", bottlesOfLucozadeConsumed);
