2e081f5bc01db7746008d04923526935c5edfc24
Caroline <carolinebirkinshaw@outlook.com>
2021-03-29 22:39:38 +0100

    add KittensName variable

--- scripts/variable.js
+++ scripts/variable.js
@@ -1,7 +1,15 @@
 var bottlesOfLucozadeConsumed = 5;
 
-alert("Bottles of Lucozade consumed:", bottlesOfLucozadeConsumed);
+alert(`Lucozade consumed: `+ bottlesOfLucozadeConsumed);
 
 console.log("Bottles of Lucozade consumed:", bottlesOfLucozadeConsumed);
 
-document.write("Bottles of Lucozade comnsumed:", bottlesOfLucozadeConsumed);
+document.write("Bottles of Lucozade consumed:", bottlesOfLucozadeConsumed);
+
+console.log('I\'d quite like to use an apostrophe')
+
+var kittensName = `Fluffy `; 
+var fullName = kittensName + `McDougle`;
+console.log(fullName);
+kittensName += `Snuggles`;
+console.log(kittensName);
