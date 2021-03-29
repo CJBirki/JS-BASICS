dd2e7f032741fb3e388ade60cf77970bebe140fc
Caroline <carolinebirkinshaw@outlook.com>
2021-03-29 22:38:10 +0100

    create moreMaths.js

---
+++ scripts/moreMaths.js
@@ -0,0 +1,6 @@
+//Use concatenation to mix strings and numbers. When you do this, JavaScript will treat the number like a string.
+
+var numberOfFruit = 6;
+var typeOfFruit = 'bananas';
+var allTheFruit = `I have ` + numberOfFruit + ` ` + typeOfFruit + `!`;
+console.log(allTheFruit);
