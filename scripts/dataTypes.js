d46bdb1091bf6076f27b58ac5664bfb77a7aa3a3
Caroline <carolinebirkinshaw@outlook.com>
2021-03-29 21:32:12 +0100

    add maths and dataTypes to scripts folder

---
+++ scripts/dataTypes.js
@@ -0,0 +1,27 @@
+//String is a string of characters
+
+var userName ='Jack Pott';
+
+//number integer or floating point
+
+var myAge = 21;
+
+//boolean true or false
+
+var catsAreBest = true;
+
+//undefined value (hasn't been defined)
+
+var favouriteThings;
+
+//null an explicitly empty value
+
+var goodPickupLines = null;
+
+/* Numbers in variables 
+NaN = Not-A-Number*/
+
+var numberOfKittens =5;
+var cutenessRating = 9.6; 
+var numberOfPuppies = 4;
+var numberOfAnimals = numberOfKittens + numberOfPuppies;
---
+++ scripts/maths.js
@@ -0,0 +1,8 @@
+var a = 10;
+var b = 5;
+
+console.log('sum', a + b);
+console.log('difference', a -b)
+console.log('multiplication', a * b);
+console.log('division', a / b);
+console.log('remainder', a % b);
